085a4e10c12c12ba436dc885334f235b7947980f
U-Александр-ПК\Александр <tiniakov.alex@gmail.com>
2019-01-26 17:34:20 +0200

    asdfsda

--- module-1-otiniako/pitcoin/miner_cli.py
+++ module-1-otiniako/pitcoin/miner_cli.py
@@ -4,6 +4,7 @@ import cmd
 import block
 from blockchain import Blockchain
 from transaction import CoinbaseTransaction
+import pickle
 
 server = xmlrpc.client.Server('http://localhost:8000')
 PEER_NODES = []
@@ -16,6 +17,31 @@ def mine():
     addr = f.readline()
     trs.append(CoinbaseTransaction(addr).ser)
     trs.append(server.get_transactions())
+    blocks = server.get_blocks()
+    '''blocks = []
+    try:
+        input = open('chain/blocks.pk1', 'rb')
+        blocks.append(pickle.load(input))
+    except:
+        print('KO')
+    blocks = []
+    try:
+        with open('chain/blocks.pk1', 'rb') as input:
+            blocks.append(pickle.load(input))
+        #return blocks
+    except:
+        #return blocks
+        print('ecxept')'''
+    print(type(blocks))
+    '''
+    f = open('chain/blockchain', 'a')
+    try:
+        blocks = f.readlines()
+        print(blocks)
+    except:
+        print('KO')
+    f.close()
+    '''
 
 if __name__ == "__main__":
     mine()
@@ -25,3 +51,15 @@ addr = f.readline()
 print(addr)
 BLOCKCHAIN = Blockchain(addr)
 '''
+'''
+def get_blocks(self):
+        blocks = []
+        with open('chain/blocks.pk1', 'rb') as input:
+            blocks.append(pickle.load(input))
+        return blocks
+
+
+    def add_block(self, block):
+        with open('chain/blocks.pk1', 'wb') as output:
+            pickle.dump(block, output, pickle.HIGHEST_PROTOCOL)
+            '''
--- module-1-otiniako/pitcoin/server.py
+++ module-1-otiniako/pitcoin/server.py
@@ -3,6 +3,8 @@
 #import xmlrpclib
 import xmlrpc.server as SimpleXMLRPCServer
 import pending_pool
+import pickle
+import os
 
 class Pitcoin:
 
@@ -17,12 +19,37 @@ class Pitcoin:
     def get_transactions(self):
         return pending_pool.get_from_mem()
 
-    def repeat(self, astr, times):
-        return astr * times
+    def get_blocks(self):
+        blocks = []
+        if os.path.isfile('chain/blocks.pk1'):
+            with open('chain/blocks.pk1', 'rb') as input:
+                blocks.append(pickle.load(input))
+        return blocks
+        
+
+    def add_block(self, block):
+        with open('chain/blocks.pk1', 'wb') as output:
+            pickle.dump(block, output, pickle.HIGHEST_PROTOCOL)
+        print('block is added')
+'''
+    def get_blocks(self):
+        f = open('chain/blockchain', 'a')
+        blocks = f.readlines()
+        f.close()
+        return blocks
+
+    def check_block(self, block):
+        return True
+
+    def add_block(self, block):
+        f = open('chain/blockchain', 'a')
+        if self.check_block(block):
+            f.write(block)
+        f.close()
+        '''
 
 
 #server = xmlrpclib.ServerProxy("http://localhost:8000", verbose=True)
 server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
 server.register_instance(Pitcoin())
-server.register_function(lambda astr: '_' + astr, '_string')
 server.serve_forever()
